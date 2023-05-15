ad6ce56d241d80b535d6e0253cc23e3e50e43e37
neodiX <neodix@ton.org>
2023-05-15 14:02:51 +0200

    dpkg in Debian doesn't support zstd compression, thus creating repacked packages for debian users

---
+++ packages/deb/repack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEBPACKAGE="${1%.deb}"
+
+[[ -z "$1" ]] && echo "Usage: $0 <package.deb>" && exit 1
+
+set -e
+ar x $DEBPACKAGE.deb
+zstd -d < control.tar.zst | xz > control.tar.xz
+zstd -d < data.tar.zst | xz > data.tar.xz
+ar -m -c -a sdsd "$DEBPACKAGE"_repacked.deb debian-binary control.tar.xz data.tar.xz
+rm debian-binary control.tar.xz data.tar.xz control.tar.zst data.tar.zst
+
+echo "Repack done. Use the following command to install."
