14a2a4c7f7e9131d1838168abda8a5c7584dd537
neodiX <neodix@ton.org>
2024-01-29 21:05:47 +0100

    minor fix

--- packages/rpm.sh
+++ packages/rpm.sh
@@ -13,7 +13,7 @@ RPM_INSTALL_PATH="$BUILD_PATH"/rpm-install
 TON_RELEASE=$5
 
 mkdir -p "$RPMBUILD_PATH"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-tar --create --file "$RPMBUILD_PATH"/SOURCES/ton.tar.gz -C "$NIX_RESULT_PATH" .
+tar --create --file "$RPMBUILD_PATH"/SOURCES/ton.tar.gz --transform 's,^,ton-dev/,' -C "$NIX_RESULT_PATH" .
 
 rpmbuild --define "_topdir $RPMBUILD_PATH" \
          --define "_prefix /usr/local" \
